4d98ad8cd5eee6c3666770b12cb4dafb0010325d
ahmadsubhan1222 <ahmadsubhan1222x@gmail.com>
2025-03-05 02:22:35 +0500

    This is the environment setup of nextJs, typeScript

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["foodblogliebe.de"],
+      },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <Image
+          className="dark:invert"
+          src="https://foodblogliebe.de/wp-content/uploads/2020/11/Foodblogliebe_Logo-Kopie-1.png"
+          alt="Foodblogliebe Logo"
+          width={100}  // Adjust as needed
+          height={40}  // Adjust as needed
+          priority
+        />
+        <h4>Einfach Muffins</h4>
+
+        
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://foodblogliebe.de/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to FBL →
+        </a>
+      </footer>
+    </div>
+  );
+}
